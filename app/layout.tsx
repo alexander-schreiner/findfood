2a654ead56d489cbae1b20b6516a51617f104d4f
alexander.schreiner <a.schreiner@spedition.de>
2023-02-09 22:28:34 +0100

    better bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        <main className="flex flex-col justify-between items-center bg-gradient-to-b from-[#081575] to-[#17182f] min-h-screen py-6 px-4">
+        <main className="flex flex-col justify-between items-center bg-gradient-to-b from-[#08031e] to-[#010320] min-h-screen py-6 px-4">
           <div className='max-w-xl'>
             {children}
           </div>
